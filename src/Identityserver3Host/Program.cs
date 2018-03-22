dea6aa40dc6baab864f7a30a175740db3c77b51a
CHRISTIAN EGWIM <christian.egwim@venturegardengroup.com>
2018-03-22 15:18:07 +0100

    INITIAL COMMIT

---
+++ src/Identityserver3Host/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin.Hosting;
+using Serilog;
+
+namespace Identityserver3Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // logging
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo
+                .LiterateConsole(outputTemplate: "{Timestamp:HH:mm} [{Level}] ({Name:l}){NewLine} {Message}{NewLine}{Exception}")
+                .CreateLogger();
+
+            // hosting identityserver
+            using (WebApp.Start<Startup>("http://localhost:5000"))
+            {
+                Console.WriteLine("server running...");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ src/Identityserver3Host/Startup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using IdentityServer3.Core.Configuration;
+using IdentityServer3.Core.Services.InMemory;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Identityserver3Host.Startup))]
+
+namespace Identityserver3Host
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+
+            var options = new IdentityServerOptions
+            {
+                Factory = new IdentityServerServiceFactory()
+                    .UseInMemoryClients(Clients.Get())
+                    .UseInMemoryScopes(Scopes.Get())
+                    .UseInMemoryUsers(new List<InMemoryUser>()),
+
+                RequireSsl = false
+            };
+
+            app.UseIdentityServer(options);
+        }
+    }
+}
