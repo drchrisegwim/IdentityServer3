a0b53b00355000c27b7c9433a89363d171bd9962
CHRISTIAN EGWIM <christian.egwim@venturegardengroup.com>
2018-03-26 14:48:58 +0100

    Added webApi and Client App

---
+++ src/IdentityServerAPI/Startup.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using IdentityServer3.AccessTokenValidation;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(IdentityServerAPI.Startup))]
+
+namespace IdentityServerAPI
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+
+
+            // accept access tokens from identityserver and require a scope of 'api1'
+            app.UseIdentityServerBearerTokenAuthentication(new IdentityServerBearerTokenAuthenticationOptions
+            {
+                Authority = "http://localhost:59377",
+                ValidationMode = ValidationMode.ValidationEndpoint,
+
+                RequiredScopes = new[] { "api1" }
+            });
+
+            // configure web api
+            var config = new HttpConfiguration();
+            config.MapHttpAttributeRoutes();
+
+            // require authentication for all controllers
+            config.Filters.Add(new AuthorizeAttribute());
+
+            app.UseWebApi(config);
+
+        }
+    }
+}
---
+++ src/IdentityServerClient/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+
+namespace IdentityServerClient
+{
+   
+    class Program
+    {
+        static TokenResponse GetClientToken()
+        {
+            var client = new TokenClient(
+                "http://localhost:59377/connect/token",
+                "silicon",
+                "F621F470-9731-4A25-80EF-67A6F7C5F4B8");
+
+            return client.RequestClientCredentialsAsync("api1").Result;
+        }
+
+        static void CallApi(TokenResponse response)
+        {
+            var client = new HttpClient();
+            client.SetBearerToken(response.AccessToken);
+
+            Console.WriteLine(client.GetStringAsync("http://localhost:59377/test").Result);
+        }
+        static void Main(string[] args)
+        {
+            CallApi(GetClientToken());
+
+            Console.ReadLine();
+        }
+    }
+}
